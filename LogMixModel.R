ca9c0d326e203a06985d0b6cadcc7f2d37ae81bc
Robert Benke <robert.benke2@gmail.com>
2018-12-09 14:11:05 +0100

    working logistic regression (groups for all people - 500)

--- LogMixModel.R
+++ LogMixModel.R
@@ -5,7 +5,7 @@ data <- as.data.table(data)
 data <- data[,IDgroup:=.GRP, by = subject_id]
 data <- data[,':='(subject_id=NULL, ALSFRS_Total= NULL)]
 data <- data[,N:=.N,by=IDgroup]
-data <- data[N>10,] #just half obs!!!
+#data <- data[N>10,] #just half obs!!!
 data <-  data[, ID := seq(1,5000)]
 data %>% summary()
 data %>% dim
@@ -27,7 +27,8 @@ results2
 plot(results2) #AIC = 1413 
 
 #Bayesian
-  library(R2WinBUGS)
+library(R2OpenBUGS)
+library(R2WinBUGS)
 
 sink("model.txt")        
 cat("model FIRST
@@ -35,12 +36,12 @@ cat("model FIRST
   # N observations
   for (i in 1:N) {
     bin_score[i] ~ dbern(p[i])
-    logit(p[i]) <- b0[group[i]] + b1[group[i]]*time 
+    logit(p[i]) <- b0[group[i]] + b1[group[i]]*time[i]
   }
-  # M groups
-  for (j in 1:M) {
-    b0[j] ~ dnorm(0,1E+2)
-    b1[j] ~ dnorm(0,1E+2)
+  # Nsubj groups
+  for (j in 1:Nsubj) {
+    b0[j] ~ dnorm(0,1000)
+    b1[j] ~ dnorm(0,1000)
   }
  }", fill = TRUE)
   sink()
@@ -51,11 +52,12 @@ cat("model FIRST
   Nsubj = length(unique(data$IDgroup))
   N = length(bin_score)
   
-  dataList = list("N","time","bin_score","Nsubj","group")
+  dataList = list("time","bin_score","group","Nsubj","N")
   params = c("b0","b1")
   
-  inits <- list(b0=rep(0.1,Nsubj),b1=rep(0.1,Nsubj))
-  
+  inits <- function(){
+     list(b0 = rnorm(Nsubj,0,11),b1 = rnorm(Nsubj,0,0.1))
+     }
   
   
   nc <- 3    #number of MCMC chains to run
@@ -63,9 +65,7 @@ cat("model FIRST
   nb <- 5000   #number of samples to discard as burn-in
   nt <- 1      #thinning rate, increase this to reduce autocorrelation
   
-  
-  attach(data)
-  bugs.out <- bugs(data=dataList,inits=NULL, parameters.to.save=params, model.file="model.txt", 
+  bugs.out <-R2WinBUGS::bugs(data=dataList,inits=NULL, parameters.to.save=params, model.file="model.txt", 
                    n.chains=nc, n.iter=ni, n.burnin=nb, n.thin=nt, debug=TRUE, DIC=TRUE,
                    bugs.directory = "F:\\WinBUGS14", working.directory=getwd())
  
